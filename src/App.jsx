16efcb6126f0953caa5c7cc3d8b9fa3cecf07999
Ethan Toups <ethantoups05@gmail.com>
2025-05-07 09:37:52 -0500

    init

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,29 @@
  * React TV is an web streaming platform that allows users to browse
  * through the episodes of a variety of different shows.
  */
+
+import { useState } from "react";
+import ShowSelection from "./shows/ShowSelection";
+import ShowDetails from "./shows/ShowDetails";
+import { tvShows } from "./shows/data.js";
+
 export default function App() {
+  const [selectedShow, setSelectedShow] = useState(null);
+
+  console.log(tvShows);
+
   return (
     <>
       <header>
         <p>React TV</p>
+        <ShowSelection
+          setSelectedShow={setSelectedShow}
+          tvShows={tvShows}
+        />
       </header>
-      <main></main>
+      <main>
+        <ShowDetails show={selectedShow}></ShowDetails>
+      </main>
     </>
   );
 }
